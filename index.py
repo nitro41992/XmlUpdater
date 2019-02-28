d48c0251869f993a9320d0dbb9e75440c6058f0b
Nick Kuchimanchi <NKuchimanchi@NJIIAD.com>
2019-02-28 17:15:41 -0500

    Fix: Refactored for OOP

--- index.py
+++ index.py
@@ -1,27 +1,42 @@
 import lxml.etree as ET
 
-tree = ET.parse('KPIMeasuresProcessingIn.xml')
-root = tree.getroot()
+def xmlParse(path):
+    tree = ET.parse(path)
+    return tree
 
-# ET.register_namespace('', "http://www.inrule.com/XmlSchema/Schema")
-# ET.register_namespace('xsd', "http://www.w3.org/2001/XMLSchema")
-# ET.register_namespace('xsi', "http://www.w3.org/2001/XMLSchema-instance")
-
-
-xsi = '{http://www.inrule.com/XmlSchema/Schema}'
-xmli = '{http://www.w3.org/2001/XMLSchema-instance}'
-codeArray = []
-updatedCodes=[]
+def xmlgetRoot(tree):
+    root = tree.getroot()
+    return root
 
 
-for DataElements in root.iter(xsi + 'DataElementDef'):
-    if DataElements.attrib[xmli + 'type'] == 'DataFolderDef' and DataElements.attrib['Name'] == 'DiagnosesCodeSets':
-        for DataElementDef in DataElements.findall(xsi + 'DataElements/'+ xsi + 'DataElementDef/'+ xsi + 'Items/' + xsi + 'ValueListItemDef/' + xsi + 'Value'):
-            if (len(DataElementDef.text) < 6 and len(DataElementDef.text) > 3 and "-" not in DataElementDef.text and "." not in DataElementDef.text) or (DataElementDef.text[0].isalpha() and len(DataElementDef.text) > 3 and "-" not in DataElementDef.text and "." not in DataElementDef.text):
-                front = DataElementDef.text[:3]
-                back = DataElementDef.text[3:]
-                new_value = (front + '.' + back)
-                DataElementDef.text = new_value
+def codeUpdate(text):
+    if (len(text) < 6 and len(text) > 3 and "-" not in text and "." not in text) or (text[0].isalpha() and len(text) > 3 and "-" not in text and "." not in text):
+        front = text[:3]
+        back = text[3:]
+        new_value = (front + '.' + back)
+        updated = new_value
+        return updated
+    else:
+        return text
+        
+
+def xmlLoop(root):
+    xsi = '{http://www.inrule.com/XmlSchema/Schema}'
+    xmli = '{http://www.w3.org/2001/XMLSchema-instance}'
+
+    for DataElements in root.iter(xsi + 'DataElementDef'):
+        if DataElements.attrib[xmli + 'type'] == 'DataFolderDef' and DataElements.attrib['Name'] == 'DiagnosesCodeSets':
+            for DataElementDef in DataElements.findall(xsi + 'DataElements/' + xsi + 'DataElementDef/' + xsi + 'Items/' + xsi + 'ValueListItemDef/' + xsi + 'Value'):
+                DataElementDef.text = codeUpdate(DataElementDef.text)
+    
+    return root
+
+def main(path):
+    tree = xmlParse(path)
+    root = xmlgetRoot(tree)
+    Updated = xmlLoop(root)
+    with open('xmltree.xml','wb') as f:
+        f.write(ET.tostring(Updated,pretty_print=True, encoding = "utf-8", method="xml", xml_declaration=True))
 
 
-tree.write('output.xml', encoding="utf-8", xml_declaration=True, method="xml")
+main('KPIMeasuresProcessingIn.xml')
